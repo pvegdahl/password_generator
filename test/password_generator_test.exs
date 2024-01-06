cb0a860ae323a2848a0c60d76cc2fab9a7ab1def
Philip Vegdahl <pvegdahl@gmail.com>
2024-01-05 21:12:01 -0500

    And the list of words is unique

--- test/password_generator_test.exs
+++ test/password_generator_test.exs
@@ -5,4 +5,8 @@ defmodule PasswordGeneratorTest do
   test "The word list has the expected number of words" do
     assert PasswordGenerator.word_list() |> Enum.count() == 4951
   end
+
+  test "The word list has no duplicates" do
+    assert PasswordGenerator.word_list() |> Enum.uniq() |> Enum.count() == 4951
+  end
 end
